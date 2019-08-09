0763e0f4cf6e1d40d085ab48ae9281cb6d294f95
gouda <eng.m.gouda@gmail.com>
2019-08-09 13:17:25 +0000

    before adding item model

--- laravel/database/migrations/2019_07_21_195556_create_tasks_table.php
+++ laravel/database/migrations/2019_07_21_195556_create_tasks_table.php
@@ -14,7 +14,21 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id',10000000);
+            $table->string('taskname');
+            $table->integer('creator');  
+            $table->integer('owner');
+            $table->integer('progress');
+            $table->integer('revenue');
+            $table->integer('evaluation');
+            $table->string('status');
+            $table->string('description');
+            $table->integer('duration');
+            $table->date('startdate');
+            $table->date('enddate');
+            $table->integer('costbaseline');
+            $table->integer('team_id'); 
+            $table->integer('parentid');   
             $table->timestamps();
         });
     }
